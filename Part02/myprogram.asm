18702c395e4bf0e592ccc7dfec3183988f59d1e0
Fraser Brown <fraserb42@gmail.com>
2016-03-24 09:33:57 +0000

    Demo Complete (Marks: 40/50) - Functionality status: test case1 for array works, test case 2 for array does not work small bug when assigining to d[1]

--- Part02/myprogram.asm
+++ Part02/myprogram.asm
@@ -1,28 +1,33 @@
-	.data
-a:	 .space 8
+	.data
+d:	 .space 40
 	 .text 
 main: 
 	li $t8,0 
 	li $t9,4 
 	mul $t8, $t8, $t9 
 	li $t9,2 
-	sw $t9, a($t8)
-	li $t8,1 
-	li $t9,4 
-	mul $t8, $t8, $t9 
-	li $t9,5 
-	sw $t9, a($t8)
+	sw $t9, d($t8)
 	li $t8,0 
 	li $t9,4 
 	mul $t8, $t8, $t9 
-	lw $t9, a($t8)
-	li $v0, 1
-	move $a0, $t9
-	syscall
+	lw $t9, d($t8)
+	addi $sp,$sp,-4
+	sw $t9,0($sp) 
+	li $t9,3
+	lw $t8,0($sp)
+	addi $sp,$sp,4 
+	add $t8, $t8, $t9
+	addi $sp,$sp,-4
+	sw $t8,0($sp) 
 	li $t8,1 
 	li $t9,4 
 	mul $t8, $t8, $t9 
-	lw $t9, a($t8)
+	lw $s0,0($sp)
+	addi $sp,$sp,4 
+	li $t8,1 
+	li $t9,4 
+	mul $t8, $t8, $t9 
+	lw $t9, d($t8)
 	li $v0, 1
 	move $a0, $t9
 	syscall
